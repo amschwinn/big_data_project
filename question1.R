2df3f62a0026d55e894daa2cbdc16de504b4dcc6
joerenner <rennerj2@students.wwu.edu>
2018-01-19 16:19:11 +0100

    bout to pull

--- question1.R
+++ question1.R
@@ -8,18 +8,14 @@ H <- hashmap(c(0),c(0))
 H$clear()
 H$empty()
 
-nrow(SATISFACTION_AUTO_BDG_2015_2016_2017)
-for (table in c(SATISFACTION_AUTO_BDG_2015_2016_2017)){
-  nrow(table)
-  x <- nrow(table)
-  x
-  for (i in seq.int(from=2, to=x)){
-    table[i,'Meta_donnee 3']
-    #if (H$has_key(table[i,'Meta_donnee 3'])){
+x <- nrow(SATISFACTION_AUTO_BDG_2015_2016_2017)
+for (i in seq.int(from=2, to=x)){
+  print(SATISFACTION_AUTO_BDG_2015_2016_2017[i,'Meta_donnee 3'])
+  #if (H$has_key(table[i,'Meta_donnee 3'])){
       
-    #}else{
+  #}else{
       
-    #}
+  #}
     
-  }
 }
+
